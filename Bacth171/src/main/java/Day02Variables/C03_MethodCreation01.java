7ac0accfc62f43eb673e546823afcf420ad7fdad
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-05-03 23:14:46 +0300

    03.05
    2023

---
+++ Bacth171/src/main/java/Day02Variables/C03_MethodCreation01.java
@@ -0,0 +1,75 @@
+package Day02Variables;
+
+public class C03_MethodCreation01 {
+    /*
+    Java'da method nasil olusturulur?
+    1)main methodun disinda classin icinde olusturulur
+                1               2               3        4     5
+    2) Access Modifier  + Return type  +  Method ismi  + () + {}
+     */
+
+    /*
+    Olusturulan methodlar nasil kullanilir?
+    1) Methodu olusturmak methodu calistirmak icin yeterli degildir.Kullanilmak istenen method
+        main method un icinden kullanilir
+    2) method'un ismini + () yazin
+    3) varsa islem yapacagınız datalari parantezin icine koyun
+        bu islem method call (method cagirma) olarak adlandirilir
+   4) method name + parametreler ==> method signature
+     */
+    public static void main(String[] args) {
+
+        int sonuc = toplamaYap(3, 5);
+        System.out.println(sonuc);
+
+        long carpmaSonucu = multiply(5, 12);
+        System.out.println(carpmaSonucu);
+
+        System.out.println(firstTwoMultiplyThirdAdd(2, 3, 4));
+
+        int dikdorgenAlanı = dikalanhesaplama(7, 8);
+        System.out.println("dikdörtgen Alan :" + dikdorgenAlanı);
+
+    int dikdorgenCevreHesapla = dikcevrehesapla(8, 9);
+        System.out.println("dikdöergen Cevre Hesapla:"+dikdorgenCevreHesapla);
+
+        int sayiToplamaCarma=toplacarma(5,6,7);
+
+        System.out.println("Topla Carpama :"+sayiToplamaCarma);
+}
+
+    public static int toplacarma(int a, int b, int c) {
+        return (a*b)+c;
+    }
+
+    public static int dikcevrehesapla(int c, int d) {
+        return 2*(c+d);
+
+    }
+
+    public static int dikalanhesaplama(int a, int b) {
+        return  a*b;
+    }
+
+    //Ornek 1: toplama islemi yapan bir method olusturunuz
+    public static int toplamaYap(int a, int b){//bu kisimda method kapsaminda kullanilmasini ve islenmesini istedigim datalari deklare ediyoruz
+        return a+b;
+        //return demek bu methodun cagrildigi yere bu degeri return et demek
+    }
+    //main method static oldugu icin main method icinde kullanacağımız method static olmalidir
+
+    //Ornek 2: 2 sayiyi carpma islemi yapan bir method olusturunuz
+    protected static long multiply(int a, int b){
+        return a*b;
+    }
+
+    //Ornek 3: Verilen 3 sayidan ilk ikisini carpan ve sonucu ucuncu sayi ile toplayan methodu olusturunuz ve kullaniniz
+    private static int firstTwoMultiplyThirdAdd(int a, int b, int c){
+        return a*b+c;
+    }
+
+        /*
+        1) Dikdortgenin alanini hesaplayan methodu olusturun ve kullaniniz
+        2) Dikdortgenin cevresini hesaplayan methodu olusturun ve kullaniniz.
+         */
+}
