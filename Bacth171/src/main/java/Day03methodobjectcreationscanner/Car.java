c79b1cd1682b53c981561367ba274e16f978fefc
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-05-05 23:30:34 +0300

    05.05
    2023

---
+++ Bacth171/src/main/java/Day03methodobjectcreationscanner/Car.java
@@ -0,0 +1,13 @@
+package Day03methodobjectcreationscanner;
+
+
+public class Car {
+    public String model="Tesla";
+    public int fiyat=20000;
+    public void hareket(){
+        System.out.println("Tesla çok hızlı hareket ediyor");
+    }
+    public void dur(){
+        System.out.println("çok güvenli şekilde duruyor");
+    }
+}
---
+++ Bacth171/src/main/java/Day03methodobjectcreationscanner/Student.java
@@ -0,0 +1,20 @@
+package Day03methodobjectcreationscanner;
+
+import java.util.Scanner;
+
+public class Student {
+
+    //variable (==> pasif özellik
+    public String name="Ali Can";
+    public  byte grade=8;
+    public  String adress="Ankara";
+
+    public void study(){
+        System.out.println("Günlük Tekrarlarını asla ihmal etmez");
+
+            }
+    public void feed(){
+        System.out.println("sağlıklı beslenir");
+    }
+
+}
