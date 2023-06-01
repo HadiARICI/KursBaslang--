f5ab6fe739a5b85e25d577785094f8dfb9636186
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-06-01 23:09:26 +0300

    01.06
    2023

---
+++ Bacth171/src/main/java/day05_Pratick/C03_WhileLoop.java
@@ -0,0 +1,31 @@
+package day05_Pratick;
+
+import day15loops.WhileLoop01;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
+public class C03_WhileLoop {
+
+    public static void main(String[] args) {
+
+
+    // Kullanıcıdan sisteme bir sayi girmesini isteyiniz
+    // kullanıcının girdigi sayının while loop kullanarak faktoriyelini bulunuz
+    //or: 5!=1*2*3*4*5
+
+    Scanner scan=new Scanner(System.in);
+        System.out.println("lütfren bir sayi giriniz");
+        int sayi=scan.nextInt();
+
+        int baslagıc=1;
+        int carpım=1;
+
+        while (baslagıc<=sayi){
+            carpım=carpım*baslagıc;
+            baslagıc++;
+
+        }
+        System.out.println(sayi+"! "+carpım);
+}
+}
