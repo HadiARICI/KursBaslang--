51e8efcfb66f3da1d61d29e3705c298f22058ad5
HadiARICI <132011080+HadiARICI@users.noreply.github.com>
2023-06-17 23:58:08 +0300

    17.06
    2023

---
+++ Bacth171/src/test/java/DayOdev/C01_ortala.java
@@ -0,0 +1,12 @@
+package DayOdev;
+
+public class C01_ortala {
+    public static void main(String[] args) {
+
+        double s1=90.30;
+        double s2=93.04;
+        double s3=89.40;
+               double top=s1+s2+s3;
+     System.out.println(top/3);
+    }
+}
---
+++ Bacth171/src/test/java/day28interfacecollections/Accord.java
@@ -0,0 +1,17 @@
+package day28interfacecollections;
+
+import day27abstractionday.Security;
+
+public class Accord implements Ac,Engine, Security {
+
+
+    @Override
+    public void cool() {
+        System.out.println("Accord cools super");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Engine runs super");
+    }
+}
